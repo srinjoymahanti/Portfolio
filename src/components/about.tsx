53cca5d143e7377407292ff5bf4555f68db0839d
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-17 10:57:52 +0530

    some changes

---
+++ src/components/about.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import { SectionHeading } from '@/components/section-heading';
+import { Skills } from '@/components/skills';
+import { useSectionInView } from '@/hooks/use-section-in-view';
+
+export const About = () => {
+  const { ref } = useSectionInView('About');
+
+  return (
+    <motion.section
+      ref={ref}
+      id="about"
+      className="my-10 flex w-full scroll-mt-28 flex-col items-center md:mb-20"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading heading="About Me" />
+      <div className="-mt-5 max-w-2xl text-center leading-7">
+        <p className="mb-4">
+          I am Srinjoy Mahanti, an Information Technology undergraduate with a solid foundation in computer science and practical experience in software development, algorithms, and problem-solving. Skilled in C, C++, Python, and JavaScript, I also work with modern frameworks like React and Next.js. My academic interests span compiler design, operating systems, cryptography, automata theory, and data science, strengthening my analytical and research skills. Beyond academics, I engage in competitive programming, project development, and open-source contributions. Passionate about bridging theory with practice, I aim to grow as a developer and researcher, driving innovation in software engineering, machine learning, and emerging technologies.
+        </p>
+        <p>
+          I&apos;m open to Job opportunities where I can contribute, learn and
+          grow. If you have a good opportunity that matches my skills and
+          experience then don&apos;t hesitate to contact me.
+        </p>
+      </div>
+      <Skills />
+    </motion.section>
+  );
+};
---
+++ src/lib/data.tsx
@@ -0,0 +1,122 @@
+import Link from 'next/link';
+
+import { buttonVariants } from '@/components/button';
+import { Icons } from '@/components/icons';
+import { cn } from '@/lib/utils';
+
+export const links = [
+  {
+    name: 'Home',
+    hash: '#home',
+  },
+  {
+    name: 'About',
+    hash: '#about',
+  },
+  {
+    name: 'Experience',
+    hash: '#experience',
+  },
+  {
+    name: 'Projects',
+    hash: '#projects',
+  },
+  {
+    name: 'Contact',
+    hash: '#contact',
+  },
+] as const;
+
+export const projectsData = [
+  {
+    image: '/images/project-2.webp',
+    title: 'Next-starter',
+    description:
+      'A Next.js starter template, packed with features like TypeScript, Tailwind CSS, Next-auth, Eslint, Stripe, testing tools and more.',
+    technologies: ['Next.js', 'Tailwind', 'Shadcn/ui', 'Next-auth', 'Prisma'],
+    links: {
+      preview: 'https://next-starter-skolaczk.vercel.app',
+      github: 'https://github.com/Skolaczk/next-starter',
+      githubApi: 'https://api.github.com/repos/Skolaczk/next-starter',
+    },
+  },
+  {
+    image: '/images/project-1.webp',
+    title: 'SocialHub',
+    description:
+      'SocialHub is a next-generation social media app developed using Next.js and Nest.js.',
+    technologies: ['Next.js', 'TypeScript', ' Nest.js', 'Tailwind', 'Prisma'],
+    links: {
+      preview: 'https://socialhub-ms.vercel.app/',
+      github: 'https://github.com/Skolaczk/SocialHub',
+      githubApi: 'https://api.github.com/repos/Skolaczk/SocialHub',
+    },
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: 'Web developer intern',
+    company: 'WEB-BOX, Tarnobrzeg PL',
+    description:
+      'During this internship, I focused on two key areas: creating Wordpress plugins and managing website content, plugins, and settings.',
+    period: '2023',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Wordpress'],
+  },
+  {
+    title: 'Frontend developer & UI designer',
+    company: 'Freelancing, remote',
+    description:
+      'As a freelancer, I specialized in graphic design using Figma and website development using Next.js with Sanity CMS. I undertook projects independently, from design conceptualization to final implementation, ensuring client satisfaction.',
+    period: '2023 - 2024',
+    technologies: ['Next.js', 'TypeScript', 'Sanity CMS', 'Tailwind'],
+  },
+  {
+    title: 'Frontend developer intern',
+    company: 'Chop-chop, remote',
+    description:
+      'During my internship at Chop-Chop, I collaborated with an experienced IT team, focusing on tasks in Next.js. This experience provided valuable insights into teamwork and advanced my proficiency in Next.js.',
+    period: '2024',
+    technologies: ['Next.js', 'React', 'TypeScript', 'Tailwind'],
+  },
+  {
+    title: 'Frontend developer & UI designer',
+    company: 'GeoScan, remote',
+    description: (
+      <>
+        At GeoScan, I collaborate with frontend, backend, and GIS specialists to
+        build{' '}
+        <Link
+          className={cn(
+            buttonVariants({ variant: 'link' }),
+            'm-0 h-fit p-0 text-base'
+          )}
+          href="https://geoscan-app.com"
+          target="_blank"
+        >
+          geoscan-app.com
+        </Link>{' '}
+        — a platform for generating real estate reports. I’m responsible for
+        developing and maintaining the frontend, as well as designing UI/UX to
+        ensure a clear and intuitive user experience.
+      </>
+    ),
+    period: '2024 - present',
+    technologies: ['Next.js', 'TypeScript', 'Tailwind', 'Supabase', 'Leaflet'],
+  },
+] as const;
+
+export const skillsData = [
+  { icon: <Icons.html className="size-12" /> },
+  { icon: <Icons.css className="size-12" /> },
+  { icon: <Icons.sass className="size-12" /> },
+  { icon: <Icons.tailwind className="size-12" /> },
+  { icon: <Icons.javascript className="size-12" /> },
+  { icon: <Icons.typescript className="size-12" /> },
+  { icon: <Icons.react className="size-12" /> },
+  { icon: <Icons.redux className="size-12" /> },
+  { icon: <Icons.nextjs className="size-12" /> },
+  { icon: <Icons.nestjs className="size-12" /> },
+  { icon: <Icons.prisma className="size-12" /> },
+  { icon: <Icons.docker className="size-12" /> },
+] as const;
